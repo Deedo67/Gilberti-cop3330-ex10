8c2d9d90eebd117930aff5a69b3b74602414b69d
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 21:31:57 -0400

    Initial commit

---
+++ src/main/java/MPE10/example/ex10.java
@@ -0,0 +1,17 @@
+package MPE10.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex10 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+    }
+}
